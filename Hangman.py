dab7e6c81c2b5644f38ef3a46a5eec983ca0e2f5
GioHernandez7 <144472624+GioHernandez7@users.noreply.github.com>
2023-09-08 12:12:41 -0400

    Add files via upload

---
+++ Hangman.py
@@ -0,0 +1,29 @@
+print("Let's play Hang Man!")
+correct_letters = ['a','m','o','n','g','u','s']
+good_letters = ['_','_','_','_','_','_','_']
+bad_letters = []
+status = "lose"
+while len(bad_letters) < 6:
+  if good_letters == correct_letters:
+    print("You Win!")
+    status = "win"
+    break
+  #if len(bad_letters) = 6:
+  print("Take a guess!: ")
+  answer = input()
+  if answer in correct_letters:
+    position = correct_letters.index(answer)
+    good_letters.pop(position)
+    good_letters.insert(position, answer)
+    print("good" + str(good_letters))
+    print("bad" + str(bad_letters))
+  else:
+    bad_letters.append(answer)
+    print("good" + str(good_letters))
+    print("bad" + str(bad_letters))
+
+if status == "lose":
+  print("You Lost!")
+
+
+
